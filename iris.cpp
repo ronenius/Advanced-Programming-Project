e4b5184a86949cfd2611d205e702c60d9e52d20a
ronenius <ronenius4@gmail.com>
2021-08-08 17:26:32 +0300

    first commit

---
+++ iris.cpp
@@ -0,0 +1,4 @@
+#include "iris.hpp"
+const char *iris::SETOSA = "setosa";
+const char *iris::VIRGINICA = "virginica";
+const char *iris::VERSICOLOR = "versicolor";
---
+++ iris.hpp
@@ -0,0 +1,11 @@
+class iris
+{
+private:
+    char *name;
+    double properties[4];
+
+public:
+    static const char *SETOSA;
+    static const char *VIRGINICA;
+    static const char *VERSICOLOR;
+};
