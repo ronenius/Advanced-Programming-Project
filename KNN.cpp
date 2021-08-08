5c0b7d6c426c9cbbc649728d6f787e1444b431b1
ronenius <ronenius4@gmail.com>
2021-08-09 02:04:19 +0300

    return statement knn

--- KNN.cpp
+++ KNN.cpp
@@ -1,45 +1,56 @@
 #include "KNN.hpp"
-static std::string getMax(std::vector<iris> irises) {
+static std::string getMax(std::vector<iris> irises)
+{
     int count1 = 0, count2 = 0, count3 = 0;
     std::string name;
-    for (int i = 0; i < irises.size(); i++) {
+    for (int i = 0; i < irises.size(); i++)
+    {
         name = irises[0].getName();
-        if (name == iris::SETOSA) {
+        if (name == iris::SETOSA)
+        {
             count1++;
         }
-        else if (name == iris::VERSICOLOR) {
+        else if (name == iris::VERSICOLOR)
+        {
             count2++;
         }
-        else {
+        else
+        {
             count3++;
         }
     }
-    int max12 = count1>count2?count1:count2;
-    int max = max12>count3?max12:count3;
-    if (max == count1) {
+    int max12 = count1 > count2 ? count1 : count2;
+    int max = max12 > count3 ? max12 : count3;
+    if (max == count1)
+    {
         return iris::SETOSA;
     }
-    if (max == count2) {
+    if (max == count2)
+    {
         return iris::VERSICOLOR;
     }
     return iris::VIRGINICA;
 }
-static void getCategory(std::vector<iris> classified, iris& unclassified, int k) {
+static void getCategory(std::vector<iris> classified, iris &unclassified, int k)
+{
     std::vector<iris> irises;
     int minIndex = 0;
     double min = unclassified.getDistance(irises[0]);
-    for (int j = 0; j < k; j++) {
-        for (int i = 1; i < classified.size(); i++) {
+    for (int j = 0; j < k; j++)
+    {
+        for (int i = 1; i < classified.size(); i++)
+        {
             double distance = unclassified.getDistance(irises[i]);
-            if (distance < min) {
+            if (distance < min)
+            {
                 min = distance;
                 minIndex = i;
             }
         }
         irises.push_back(classified[minIndex]);
-        if (classified.size()==1)
+        if (classified.size() == 1)
             break;
-        classified[minIndex] = classified[classified.size()-1];
+        classified[minIndex] = classified[classified.size() - 1];
         classified.pop_back();
         minIndex = 0;
         min = unclassified.getDistance(irises[0]);
@@ -47,8 +58,11 @@ static void getCategory(std::vector<iris> classified, iris& unclassified, int k)
     std::string name = getMax(irises);
     unclassified.setName(name);
 }
-std::vector<iris> getCategories(std::vector<iris> classified, std::vector<iris> unclassified, int k) {
-    for (int i = 0; i < unclassified.size(); i++) {
+std::vector<iris> getCategories(std::vector<iris> classified, std::vector<iris> unclassified, int k)
+{
+    for (int i = 0; i < unclassified.size(); i++)
+    {
         getCategory(classified, unclassified[i], k);
     }
+    return unclassified;
 }
