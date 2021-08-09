e1ae93d05040f90e6bf7fcef66cc0ea2a7880696
hugsehbd <yoadyw@gmail.com>
2021-08-09 21:18:28 +0300

    documentation 2

--- KNN.hpp
+++ KNN.hpp
@@ -1,2 +1,3 @@
 #include "iris.hpp"
+//returns a vector similar to unclassified, but with species. the species are calculated with KNN algorithm.
 std::vector<iris> getCategories(std::vector<iris> classified, std::vector<iris> unclassified, int k);
