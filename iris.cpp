a0be48a29824737eb7afaa3b2d277d77c877b571
hugsehbd <yoadyw@gmail.com>
2021-08-09 21:16:41 +0300

    documentation1

--- iris.cpp
+++ iris.cpp
@@ -3,21 +3,27 @@ std::string iris::SETOSA = "setosa";
 std::string iris::VIRGINICA = "virginica";
 std::string iris::VERSICOLOR = "versicolor";
 std::string iris::UNDEFINED = "undefined";
+//initializes using the data and does nothing else.
 iris::iris(std::string name, std::vector<double> properties) : name(name), properties(properties){};
+//returns the name (category).
 std::string iris::getName()
 {
     return name;
 }
+//sets the name(category).
 void iris::setName(std::string name)
 {
     this->name = name;
 }
+//returns the properties of the flower.
 std::vector<double> iris::getProperties()
 {
     return properties;
 }
+//returns the distance to the other flower.
 double iris::getDistance(iris other)
 {
+    //using the euclidean distance.
     return sqrt((properties[FIRST_FEATURE] - other.getProperties()[FIRST_FEATURE]) *
                     (properties[FIRST_FEATURE] - other.getProperties()[FIRST_FEATURE]) +
                 (properties[SECOND_FEATURE] - other.getProperties()[SECOND_FEATURE]) *
