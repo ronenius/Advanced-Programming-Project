f41e6426dec6abb19607795af34cf6c668c80f0d
ronenius <ronenius4@gmail.com>
2021-08-27 00:37:49 +0300

    implemented tcp server main

--- ass2/src/server/tcpServer/main.cpp
+++ ass2/src/server/tcpServer/main.cpp
@@ -1,5 +1,11 @@
 #include "tcpSocket.hpp"
 #include "tcpServer.hpp"
+#include "irisBuilder.hpp"
+#include "knnClassifier.hpp"
+
+#define NUM_PROPERTIES 4
+#define K 5
+
 int main()
 {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -9,7 +15,19 @@ int main()
     }
     tcpServer TCPServer(sock);
     TCPServer.bind();
-    TCPServer.listen();
-    int clientSock = TCPServer.accept();
-    tcpSocket 
+    while (true)
+    {
+        TCPServer.listen();
+        int clientSock = TCPServer.accept();
+        irisBuilder builder;
+        stringIO serverIO(&builder);
+        knnClassifier classif;
+        std::vector<classifiable *> classifiedData = serverIO.importStringToVector(serverIO.importFileToString("../../../data/classified.csv"), NUM_PROPERTIES);
+        tcpSocket tcp(clientSock, &serverIO, NUM_PROPERTIES, K, &classif, classifiedData);
+        tcp.send("Connection was successful. Please choose your operation:");
+        std::string message = tcp.receive();
+        std::string newData = tcp.classify(message);
+        tcp.send(newData);
+        tcp.closeSocket();
+    }
 }
